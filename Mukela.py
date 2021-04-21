83e680fd3c6a1081e3b86b304f55a355b0f6e5bc
Mukela12 <Mukelathegreat@gmail.com>
2021-04-22 07:29:03 +0800

    added to python file

--- Mukela.py
+++ Mukela.py
@@ -1,2 +1,17 @@
 Mukela  = int(input("Enter number:"))
-print(Mukela)
+def add(Mukela):
+    Mukela = Mukela + 1000
+    print(Mukela)
+add(Mukela)
+# alright another part
+
+class Thegreat:
+    def __init__(self, Mukela):
+        self.Mukela = Mukela
+        self.calculate()
+
+    def calculate(self):
+        self.Mukela = self.Mukela + 3000
+        print(self.Mukela)
+
+Thegreat(Mukela)
